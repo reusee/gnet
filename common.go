bae84775afb2fe8aea254b48e234790a1fcf826c
reus <reusee@gmail.com>
2012-11-04 23:29:43 +0800

    add interval ping

--- common.go
+++ common.go
@@ -6,6 +6,9 @@ import (
 
 const (
   CHAN_BUF_SIZE = 2 ^ 19
+
+  TYPE_PING = byte(0)
+  TYPE_DATA = byte(1)
 )
 
 func p(f string, vars ...interface{}) {
--- conn_pool.go
+++ conn_pool.go
@@ -5,6 +5,7 @@ import (
   "bytes"
   "encoding/binary"
   "io"
+  "time"
 )
 
 type ConnPool struct {
@@ -45,56 +46,79 @@ func (self *ConnPool) start() {
 //TODO handle read and write error
 
 func (self *ConnPool) startConnWriter(conn *net.TCPConn) {
+  keepAliveTicker := time.NewTicker(time.Second * 10)
   for {
-    packet := <-self.sendChan
-    l := len(packet)
-    toSend := make([]byte, 4 + l)
-    packetLenBuf := new(bytes.Buffer)
-    binary.Write(packetLenBuf, binary.BigEndian, uint32(l))
-    copy(toSend[:4], packetLenBuf.Bytes()[:4])
-    xorSlice(packet, toSend[4:], l, l % 8, self.byteKeys, self.uint64Keys)
-    _, err := conn.Write(toSend)
-    if err != nil {
-      if self.badConn != nil {
-        self.badConn <- true
+    select {
+    case packet := <-self.sendChan:
+      l := len(packet)
+      toSend := make([]byte, 1 + 4 + l)
+      toSend[0] = TYPE_DATA
+      packetLenBuf := new(bytes.Buffer)
+      binary.Write(packetLenBuf, binary.BigEndian, uint32(l))
+      copy(toSend[1:5], packetLenBuf.Bytes()[:4])
+      xorSlice(packet, toSend[5:], l, l % 8, self.byteKeys, self.uint64Keys)
+      _, err := conn.Write(toSend)
+      if err != nil {
+        if self.badConn != nil {
+          self.badConn <- true
+        }
+        break
       }
-      break
+    case <-keepAliveTicker.C:
+      conn.Write([]byte{TYPE_PING})
     }
   }
 }
 
 func (self *ConnPool) startConnReader(conn *net.TCPConn) {
   for {
-    var packetLen uint32
-    err := binary.Read(conn, binary.BigEndian, &packetLen)
+    var packetType byte
+    err := binary.Read(conn, binary.BigEndian, &packetType)
     if err != nil {
       if self.badConn != nil {
         self.badConn <- true
       }
       break
     }
-    buf := make([]byte, packetLen)
-    _, err = io.ReadFull(conn, buf)
-    if err != nil {
-      if self.badConn != nil {
-        self.badConn <- true
-      }
-      break
-    }
-    decrypted := make([]byte, packetLen)
-    xorSlice(buf, decrypted, int(packetLen), int(packetLen) % 8, self.byteKeys, self.uint64Keys)
-    var sessionId uint64
-    binary.Read(bytes.NewReader(decrypted[:8]), binary.BigEndian, &sessionId)
-    var serial uint32
-    binary.Read(bytes.NewReader(decrypted[8:12]), binary.BigEndian, &serial)
-    packet := decrypted[12:]
 
-    session := self.sessions[sessionId]
-    if session == nil { // create new session
-      session = newSession(sessionId, self.sendChan)
-      self.sessions[sessionId] = session
-      self.newSessionChan <- session
+    switch packetType {
+
+    case TYPE_DATA:
+      var packetLen uint32
+      err := binary.Read(conn, binary.BigEndian, &packetLen)
+      if err != nil {
+        if self.badConn != nil {
+          self.badConn <- true
+        }
+        break
+      }
+
+      buf := make([]byte, packetLen)
+      _, err = io.ReadFull(conn, buf)
+      if err != nil {
+        if self.badConn != nil {
+          self.badConn <- true
+        }
+        break
+      }
+
+      decrypted := make([]byte, packetLen)
+      xorSlice(buf, decrypted, int(packetLen), int(packetLen) % 8, self.byteKeys, self.uint64Keys)
+      var sessionId uint64
+      binary.Read(bytes.NewReader(decrypted[:8]), binary.BigEndian, &sessionId)
+      var serial uint32
+      binary.Read(bytes.NewReader(decrypted[8:12]), binary.BigEndian, &serial)
+      packet := decrypted[12:]
+
+      session := self.sessions[sessionId]
+      if session == nil { // create new session
+        session = newSession(sessionId, self.sendChan)
+        self.sessions[sessionId] = session
+        self.newSessionChan <- session
+      }
+      session.packetChan <- Packet{serial: serial, packet: packet}
+
+    case TYPE_PING: // just a ping
     }
-    session.packetChan <- Packet{serial: serial, packet: packet}
   }
 }
